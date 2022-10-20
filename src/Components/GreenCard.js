c9439c38bbac985056da8a80c5971254f51abb99
zertelashvili <111557980+zertelashvili@users.noreply.github.com>
2022-10-20 19:20:44 +0400

    done

--- src/Components/GreenCard.js
+++ src/Components/GreenCard.js
@@ -6,10 +6,16 @@ export default function GreenCard(props) {
 
     return (
         <div className='greencard'>
-            <p className='tip'>Tip Amount</p>
-            <p className='pers'>/ person</p> <span className='price'>${props.result1}</span>
-            <p className='total'>Total</p>
-            <p className='pers2'>/ person</p><span className='price2'>${props.result2}</span>
+            <div className='first'>
+                <p className='tip'>Tip Amount</p>
+                <p className='pers'>/ person</p>
+            </div>
+            <span className='price'>${props.result1}</span>
+            <div className='second'>
+                <p className='total'>Total</p>
+                <p className='pers2'>/ person</p>
+            </div>
+            <span className='price2'>${props.result2}</span>
             {props.result1 === '0.00' ? <div disabled className='reset'>RESET</div> : <div onClick={() => { props.clickReset(); }} className='reset'>RESET</div>}
 
         </div>
